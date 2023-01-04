d052f3744fcdd810ea5f453140888d830572d2bd
Matt01270 <matt.92.online@googlemail.com>
2023-01-04 10:21:48 -0500

    updating script

--- script.js
+++ script.js
@@ -6,43 +6,54 @@
 
 const movieApp = {}; 
 
-movieApp.apiUrl = "https://api.themoviedb.org/3/movie/popular"; 
-movieApp.posterUrl = "https://image.tmdb.org/t/p/"
-movieApp.apiKey = 'fb179007c6601d74f9777f1516562eb1'; 
+movieApp.apiUrl = "https://api.themoviedb.org/3/movie/top_rated"; 
+movieApp.apiKey = 'fb179007c6601d74f9777f1516562eb1';
 
-const url = new URL(movieApp.apiUrl); 
-url.search = new URLSearchParams({ 
-    api_key: movieApp.apiKey, 
-})  
-
-posterUrl = new URL(movieApp.posterUrl); 
-
-posterUrl.search = new URLSearchParams({ 
-   
-})
-
-console.log(url);
-console.log(posterUrl);
-movieApp.init = () => { 
-
+movieApp.getRandomMovie = () => {
+    const url = new URL(movieApp.apiUrl); 
+    url.search = new URLSearchParams({ 
+        api_key: movieApp.apiKey,
+    }); 
+    
     fetch(url) 
-    .then((response) => { 
-    return response.json();
+    .then(response => { 
+        return response.json();
     }) 
-    .then((jsonResponse) => { 
-    console.log(jsonResponse); 
-    
+    .then(jsonResponse => { 
+        
+        const popularEnMovies = jsonResponse.results.filter ( movies => {
+            return movies.original_language === 'en';
+        });
+        const randomMovie = popularEnMovies[Math.floor(Math.random()*popularEnMovies.length)];
+        movieApp.randomBackdrop = `https://www.themoviedb.org/t/p/original/${randomMovie.backdrop_path}`
+        img = document.querySelector('img');
+        img.src = movieApp.randomBackdrop;
+    });
+};
 
-    })
+movieApp.init = () => { 
+  movieApp.getRandomMovie();
+};
 
-    
+// movieApp.getPoster = () =>{
+//     console.log(movieApp.randomMovie)
+//     movieApp.backdropUrl = `https://api.themoviedb.org/3/movie/${movieApp.randomMovie}/images`
+//     const backdropUrl = new URL(movieApp.backdropUrl);
 
-    
+//     backdropUrl.search = new URLSearchParams({
+//         api_key: movieApp.apiKey
+//     });
+
+//     fetch (backdropUrl)
+//     .then(response => {
+//         return response.json();
+//     })
+//     .then(jsonResponse => {
+//         console.log(jsonResponse);
+//     })
+// }
 
 
-} 
-
- 
 
 
 
